725027c790800208dc476f9f1a3dfca64ccf1ca5
vaibhavrsawant-glitch <vaibhavrsawant@gmail.com>
2026-07-24 18:32:50 +0530

    Update hellovaibhav.java

--- hellovaibhav.java
+++ hellovaibhav.java
@@ -1,5 +1,5 @@
 public class hellovaibhav {
     public static void main(String[] args) {
-        System.out.println("hello vaibhav in world of java");
+        System.out.println("hello vaibhav in world of java and jenkin");
     }
 }
